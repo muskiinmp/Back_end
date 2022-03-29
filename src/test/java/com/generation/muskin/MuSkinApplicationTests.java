35078867daca50b7689fd193bb8d492be52591ac
Andre <andrefelipe.rodri@gmail.com>
2022-03-29 10:50:37 -0300

    Criar o Projeto Spring

---
+++ src/test/java/com/generation/muskin/MuSkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.muskin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuSkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
