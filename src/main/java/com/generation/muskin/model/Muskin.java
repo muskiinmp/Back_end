a01f0d5e6fe68307c6a5885f46f6f09e267dcab6
LipeJobs <felipecalux@hotmail.com>
2022-03-29 11:56:17 -0300

    Primeiro Model
    Feito no dia 29/03/2022

---
+++ src/main/java/com/generation/muskin/model/Muskin.java
@@ -0,0 +1,43 @@
+package com.generation.muskin.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_categoria")
+public class Muskin {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	@NotBlank(message = "A categoria é um dado obrigatório.")
+	@Size(min = 10, max =50, message = "A categoria precisa ter no mínimo 10 caracteres e no máximo 50.")
+	private String categoria;
+	@NotNull
+	@Size(min = 10, max = 500, message = "A descrição tem que ter no mínimo 10 caracteres e no máximo 500")
+	private String descricao;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
