82ff5a338044ee57e6a50c03c0c63374d8c9ad32
Jaiane Oliveira <blue.love02c@gmail.com>
2022-04-01 13:50:57 -0300

    atualizando categoria

--- src/main/java/com/generation/muskin/model/Categoria.java
+++ src/main/java/com/generation/muskin/model/Categoria.java
@@ -1,13 +1,19 @@
 package com.generation.muskin.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -25,6 +31,11 @@ public class Categoria {
 	@Size(min = 10, max = 500, message = "A descrição tem que ter no mínimo 10 caracteres e no máximo 500")
 	private String descricao;
 
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto>produto;
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -47,6 +58,14 @@ public class Categoria {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 }
