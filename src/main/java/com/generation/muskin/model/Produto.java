a8d2d7f17781da5442f990db57229aae7f6729f0
William <86013044+WilliamVSan@users.noreply.github.com>
2022-04-01 14:00:42 -0300

    Removendo pastas adicionais.

---
+++ src/main/java/com/generation/muskin/model/Produto.java
@@ -0,0 +1,89 @@
+package com.generation.muskin.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Nome é obrigatório")
+	public String nome;
+
+	public int quantidade;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@NotNull(message = " Preço é obrigatório")
+	@Positive(message = "Preço precisa ser acima de R$0,00")
+	public BigDecimal preco;
+
+	@Size(max = 1000, message = " descrição 0/1000")
+	public String descricao;
+
+	@Size(max = 45, message = "Tamanho 0/45 ")
+	public String tamanho;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+
+}
