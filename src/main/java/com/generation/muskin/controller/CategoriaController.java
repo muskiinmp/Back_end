a1e1454cf8a549b51eed0677076bd69b1a3184f6
Jaiane Oliveira <blue.love02c@gmail.com>
2022-04-01 11:08:28 -0300

    subindo categoria

---
+++ src/main/java/com/generation/muskin/controller/CategoriaController.java
@@ -0,0 +1,81 @@
+package com.generation.muskin.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.muskin.model.Categoria;
+import com.generation.muskin.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>>getAll(){
+		return ResponseEntity.ok().body(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Categoria>> getById(@PathVariable Long id){
+		if (repository.existsById(id)) {
+			return ResponseEntity.ok().body(repository.findById(id));
+		}
+		else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@GetMapping("/categoria/{categoria}")
+	public ResponseEntity<List<Categoria>>getByCategoria(@PathVariable String categoria){
+		return ResponseEntity.ok().body(repository.findAllByCategoriaContainingIgnoreCase(categoria));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria>post(@Valid @RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+		
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria>put(@Valid @RequestBody Categoria categoria){
+		if (repository.existsById(categoria.getId())) {
+			return ResponseEntity.ok().body(repository.save(categoria));
+		}
+		else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?>delete(@PathVariable Long id){
+		if(repository.existsById(id)) {
+			repository.deleteById(id);
+			return ResponseEntity.noContent().build();
+		}
+		else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	
+	
+}
